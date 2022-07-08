f40c70213a81fa46dee3ab8a0157b3e1db1f3174
MyUsername <romamargine@gmail.com>
2022-07-08 16:05:58 +0200

    Add pages link for admin page, home guest page and logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,8 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('admin.dashboard')}}">Admin</a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
@@ -76,7 +78,7 @@
     <main class="py-4">
         @yield('content')
     </main>
-    
+
 </body>
 
 </html>
